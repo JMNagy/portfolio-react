249b4a267267267b3c191cbe4153a84f49a6ad85
John-Michael <johnmcnagy@gmail.com>
2022-10-24 17:40:54 -0400

    Added cube animation to about page and fixed text.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -30,11 +39,29 @@ const About = () => {
           I'm quiet confident, naturally curious, and perpetually working on
           improving my chops one design problem at a time.
         </p>
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
+        <p>Place holder text.</p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
       </div>
     </div>
   )
