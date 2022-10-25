64ef04e7089581fa0d5acbd341e390fb561dde2b
John-Michael <johnmcnagy@gmail.com>
2022-10-25 03:17:33 -0400

    Added contact form and emailJS functionality.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,41 @@
-import './index.scss'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'default_service',
+        'template_zatipr2',
+        form.current,
+        'bco5jTbCHa226gTlS'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -28,6 +53,61 @@ const Contact = () => {
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
           </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          John-Michael Nagy,
+          <br />
+          United States,
+          <br />
+          Burma Road <br />
+          Southbury <br />
+          <br />
+          <span>johnmcnagydev@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[41.4793, -73.2134]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[41.4793, -73.2134]}>
+              <Popup>
+                John-Michael lives here, come over for a cup of coffee :)
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="line-scale" />
