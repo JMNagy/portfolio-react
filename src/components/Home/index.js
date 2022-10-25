e613d5cb532e4364abb3265b660cfaade758aff3
John-Michael <johnmcnagy@gmail.com>
2022-10-25 01:20:02 -0400

    Added loading animation to home and about pages,

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import Loader from 'react-loaders'
 
 //<img src={LogoTitle} alt="developer" />
 
@@ -47,34 +48,37 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <span className={`${letterClass} _13`}>,</span>
-          <br />
-          <span className={`${letterClass} _14`}>I</span>
-          <span className={`${letterClass} _15`}>'</span>
-          <span className={`${letterClass} _16`}>m</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={17}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={30}
-          />
-        </h1>
-        <h2>Frontend Developer / Javascript</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <span className={`${letterClass} _13`}>,</span>
+            <br />
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'</span>
+            <span className={`${letterClass} _16`}>m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={17}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={30}
+            />
+          </h1>
+          <h2>Frontend Developer / Javascript</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="line-scale" />
+    </>
   )
 }
 
