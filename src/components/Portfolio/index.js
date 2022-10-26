15083344c497a0f3e605affed3c0892925ebca68
John-Michael <johnmcnagy@gmail.com>
2022-10-26 17:56:19 -0400

    Added animation to Portfolio page title.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import './index.scss'
 import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <>
       <div className="container portfolio-page">
-        <h1 className="page-title">Portfolio</h1>
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio()}</div>
       </div>
       <Loader type="line-scale" />
     </>
