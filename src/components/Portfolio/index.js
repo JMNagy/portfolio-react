de6e4e4837a3113ab5b2d7d30b99bc3bd23080a7
John-Michael <johnmcnagy@gmail.com>
2022-10-27 15:53:34 -0400

    Created data folder with json file.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,15 +3,36 @@ import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  console.log(portfolioData)
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {' '}
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -23,7 +44,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div>{renderPortfolio()}</div>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="line-scale" />
     </>
