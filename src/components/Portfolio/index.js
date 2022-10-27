6b886a0459ca514abb67f2b2305d160d9476557b
John-Michael <johnmcnagy@gmail.com>
2022-10-27 17:16:59 -0400

    Finished styles for portfolio page.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -27,6 +27,13 @@ const Portfolio = () => {
                 className="portfolio-image"
                 alt="portfolio"
               />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           )
         })}
