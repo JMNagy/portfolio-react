ff62a12d622ad9652f03b522f9c88969607fd0a3
John-Michael <johnmcnagy@gmail.com>
2022-11-09 14:56:56 -0500

    Nav bar now works with mobile and can close.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,66 +8,83 @@ import {
   faSuitcase,
   faUser,
   faBars,
+  faClose,
 } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Lottie from 'lottie-react'
 import ReactLogo from '../../assets/images/react_l.json'
+import { useState } from 'react'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/portfolio-react/home">
-      <Lottie animationData={ReactLogo} />
-      <img className="sub-logo" src={LogoSubtitle} alt="johnmichael" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/portfolio-react/home">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="about-link"
-        to="/portfolio-react/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio-react/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="contact-link"
-        to="/portfolio-react/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/john-michael-nagy-03944218a/"
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/portfolio-react/home">
+        <Lottie animationData={ReactLogo} />
+        <img className="sub-logo" src={LogoSubtitle} alt="johnmichael" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/portfolio-react/home"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href="https://github.com/JMNagy">
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-    <FontAwesomeIcon
-      icon={faBars}
-      color="#ffd700"
-      size="3x"
-      className="hamburger-icon"
-    />
-  </div>
-)
-
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="about-link"
+          to="/portfolio-react/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio-react/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/portfolio-react/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/john-michael-nagy-03944218a/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/JMNagy">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
 export default Sidebar
